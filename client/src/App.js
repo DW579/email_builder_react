5912c579570b6cb3d1ac1edf468c1b5548ade914
Dustin Wurtz <dtwurtz@gmail.com>
2019-03-22 15:22:42 -0700

    Implemented Footer

--- client/src/App.js
+++ client/src/App.js
@@ -25,29 +25,62 @@ import './App.css';
 //   }
 // }
 
-class Nav extends Component {
-  state = { data: [] }
+// class Nav extends Component {
+//   state = { data: [] }
 
-  componentDidMount() {
-    fetch('/data')
-      .then(res => res.json())
-      .then(data => this.setState({ data }));
-  }
+//   componentDidMount() {
+//     fetch('/data')
+//       .then(res => res.json())
+//       .then(data => this.setState({ data }));
+//   }
+//   render() {
+//     return (
+//       <div className="Nav">
+//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//           <img src={logo} alt="Oracle" />
+//         </nav>
+//         <div className="Data">
+//           <h1>Data</h1>
+//           {this.state.data.map(data =>
+//             <div key={data.id}>{data.name}</div>
+//           )}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+class Nav extends Component {
   render() {
     return (
       <div className="Nav">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <img src={logo} alt="Oracle" />
         </nav>
-        <div className="Data">
-          <h1>Data</h1>
-          {this.state.data.map(data =>
-            <div key={data.id}>{data.name}</div>
-          )}
-        </div>
       </div>
     );
   }
 }
 
-export default Nav;
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <div className="footer fixed-bottom">Copyright &copy;2019 Oracle</div>
+      </div>
+    )
+  }
+}
+
+class Body extends Component {
+  render() {
+    return (
+      <div className="Body">
+        <Nav></Nav>
+        <Footer></Footer>
+      </div>
+    )
+  }
+}
+
+export default Body;
