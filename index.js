860a71a06ac4b17544ef0ec54de672e4fd25b637
Dustin Wurtz <dtwurtz@gmail.com>
2019-03-22 10:07:19 -0700

    Test on server side data

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // All API endpoints
 app.get('/', (req, res) => {
+  console.log("Data from the server");
   res.send("Homepage is here");
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  // res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.send("Catch all page");
 });
 
 const port = process.env.PORT || 5000;
